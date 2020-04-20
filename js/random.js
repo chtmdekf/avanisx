50463b0a0d26399f12c6a6e270be0f453327e561
chtmdekf <64016037+chtmdekf@users.noreply.github.com>
2020-04-20 21:45:44 +0900

    Create random.js

---
+++ js/random.js
@@ -0,0 +1,5 @@
+        function random(){
+        let random = Math.floor((Math.random() * 10) + 1);
+        console.log(random);
+        document.getElementById('keytext').innerHTML = 'KEY2';
+        }
