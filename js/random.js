b883bf901eddab20daa12e16767aea149f5eb452
chtmdekf <64016037+chtmdekf@users.noreply.github.com>
2020-04-20 21:58:58 +0900

    Delete random.js

--- js/random.js
+++
@@ -1,5 +0,0 @@
-        function random(){
-        let random = Math.floor((Math.random() * 10) + 1);
-        console.log(random);
-        document.getElementById('keytext').innerHTML = random;
-        }
